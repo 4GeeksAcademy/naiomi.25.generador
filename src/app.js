119ce7812f15e790ced5e620cb5c38e06f0a92ea
naiomi25 <naiomidiaz.her@gmail.com>
2025-02-01 13:33:51 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,25 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
+  let quien = ["mama", "profesora", "hamster", "jefa"];
+  let accion = ["robó", "chocó", "gritó", "saltó"];
+  let qué = ["trabajo", "ascensor", "establo", "parque"];
+  let cuándo = [
+    "cuando salia de casa",
+    "cuando estaba dormida",
+    "mientras conducia",
+    "mientras comia"
+  ];
+
+  let quienAleatorio = quien[Math.floor(Math.random() * quien.length)];
+  let accionAleatorio = accion[Math.floor(Math.random() * accion.length)];
+  let quéAleatorio = qué[Math.floor(Math.random() * qué.length)];
+  let cuándoAleatorio = cuándo[Math.floor(Math.random() * cuándo.length)];
+
+  let excusa = `Mi ${quienAleatorio} ${accionAleatorio} en el ${quéAleatorio} ${cuándoAleatorio} `;
+  console.log(excusa);
+
+  document.getElementById("excuse").innerHTML = excusa;
   //write your code here
   console.log("Hello Rigo from the console!");
 };
